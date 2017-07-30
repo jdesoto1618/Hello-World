516ad3ba204ccf95db71c2b6ca5dc0d7479f17d2
Joe DeSoto <modulusman@gmail.com>
2017-07-29 20:54:07 -0700

    Updated comments, names. Program still won't make unique groups of 2

--- Randomizer.js
+++ Randomizer.js
@@ -1,11 +1,13 @@
-var people = ["Joe", "Amy", "Garrett", "Mark", "Matt", "Soldo", "Saliana", "Rob", "Bri", "FilipinoDude", "ThreeKiddos", "ExtraPerson"];
-// Range is 0 to 10, since using floor the program can't reach 12. Thus the range is 0 to 11, 12 total possibilites
-for (i = 0; i < 6; i++) { // Run this six times to generate six groups
-  var pick1 = Math.floor(Math.random()*12);
-  var pick2 = Math.floor(Math.random()*12);
-  // Change a number if they are the same
+var people = ["Joe", "Amy", "Garrett", "Mark", "Matt", "Sandro", "Sharmila", "Rob", "Bri", "Rithy"];
+// Range is 0 to 10, since using floor the program can't reach 11. Thus the range is 0 to 9, ten total possibilites
+for (i = 0; i < 5; i++) { // Run this six times to generate six groups
+  var pick1 = Math.floor(Math.random()*11);
+  var pick2 = Math.floor(Math.random()*11);
+  // Change one of the numbers if they are the same
   while (pick1 === pick2) {
-    pick2 = Math.floor(Math.random()*12);
+    pick2 = Math.floor(Math.random()*11);
   }
   console.log(people[pick1] + " and " + people[pick2] + " are a group!");
 } //ends for loop
+
+//This program is a good start, but ultimately incorrect. People already selected for a group can and often are palced in another group. The while loop only prevents the same person from being paired with themselves. 
